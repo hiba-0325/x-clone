37f5e4385004b689c1390fb3b8f3aa3e70ea2407
hiba-0325 <hibakm72@gmail.com>
2025-02-08 21:34:41 +0530

    add : (index.js)

--- server/index.js
+++ server/index.js
@@ -1,22 +1,35 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import connectDB from "./config/mongoDb.js";
 import authRoute from "./routes/authRoutes.js";
+import userRoute from "./routes/userRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+import notifRoute from "./routes/notifRoutes.js";
 
 const app = express();
 dotenv.config();
-connectDB();
 
+connectDB();
 app.use(express.json());
 
 // Example route
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("Hello World");
+});
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/messages", messageRoute);
+app.use("/notifications", notifRoute);
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Route not found" });
 });
 
-app.use("/api/auth", authRoute);
-
-const port = process.env.PORT || 3005; // Default to 3005 if PORT is not set in .env
+const port = process.env.PORT || 3005;
 app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
