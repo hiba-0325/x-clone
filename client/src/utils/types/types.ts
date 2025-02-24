f66cb6c7225aed405bcdac941315a46eeda38cab
hiba-0325 <hibakm72@gmail.com>
2025-02-24 22:19:37 +0530

    feat(client):added types

--- client/src/utils/types/types.ts
+++ client/src/utils/types/types.ts
@@ -3,31 +3,38 @@ export type UserDetails = {
   userName: string;
   email: string;
   name?: string;
-  following?: string[];
-  followers?: string[];
+  following?: { _id: string; userName: string; pfp?: string }[];
+  followers?: { _id: string; userName: string; pfp?: string }[];
   pfp?: string;
+  tweets?: string[];
   createdAt?: string;
 };
-
 export type Comment = {
   _id: string;
   user: UserDetails;
-  text: string;
+  content: string;
   createdAt: string;
+  tweet?: string;
 };
 
 export interface TweetData {
   _id: string;
   user: UserDetails;
+//   pfp: string;
+//  userName: string;
   text: string;
   createdAt: string;
   media?: string[];
-  comments?: string[];
-  likes?: string[];
-  repost?: string[];
-  saved?: string[];
+  comments?: {
+    _id: string;
+    user: UserDetails;
+    content: string;
+    createdAt: string;
+  }[];
+  likes?: { _id: string; userName: string; pfp?: string }[];
+  reposts?: { _id: string; userName: string; pfp?: string }[];
+  saved?: { _id: string; userName: string; pfp?: string }[];
 }
-
 export interface CommentData {
   _id: string;
   user: UserDetails;
@@ -36,13 +43,14 @@ export interface CommentData {
 }
 
 export type FollowUser = {
-  _id?: string;
+  _id: string;
   name?: string;
-  userName?: string;
+  userName: string;
   pfp?: string;
   bio?: string;
-  followers?: [];
-  following?: [];
+  followers?: { _id: string; userName: string; pfp?: string }[];
+  following?: { _id: string; userName: string; pfp?: string }[];
+  tweets?: string[];
 };
 
 export interface User {
@@ -53,11 +61,56 @@ export interface User {
   password?: string;
   header: string;
   bio?: string;
-  following?: string[];
-  followers?: string[];
+  following?: { _id: string; userName: string; pfp?: string }[];
+  followers?: { _id: string; userName: string; pfp?: string }[];
   pfp?: string;
   updatedAt?: string;
   location?: string;
   web?: string;
   tweets: string[];
+  likedTweets?: string[];
+  savedTweets?: string[];
 }
+
+export interface Message {
+  _id: string;
+  sender: string;
+  receiver: string;
+  content: string;
+  read: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// interfaces/notification.ts
+
+export interface Notification {
+  _id: string; 
+  acceptor: {
+    _id: string;
+    name: string; 
+    pfp?: string; 
+  };
+  sender: {
+    _id: string; 
+    name: string; 
+    pfp?: string; 
+  };
+  type: "follow" | "like" | "repost" | "comment" | "message" | "save"; 
+  read: boolean; 
+  media?: string;
+  createdAt: string; 
+  updatedAt: string; 
+}
+
+// interface TweetProps {
+//   _id: string;
+//   user: UserDetails;
+//   text: string;
+//   createdAt: string;
+//   media?: string[];
+//   comments?: { _id: string; user: UserDetails; content: string; createdAt: string }[];
+//   likes?: { _id: string; userName: string; pfp?: string }[];
+//   reposts?: { _id: string; userName: string; pfp?: string }[];
+//   saved?: { _id: string; userName: string; pfp?: string }[];
+// }
