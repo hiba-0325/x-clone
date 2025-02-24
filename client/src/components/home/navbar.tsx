2fd8d7e58c2a4b778d3135e97dbf17e2a4f0e70d
hiba-0325 <hibakm72@gmail.com>
2025-02-25 00:45:27 +0530

    feat(client):updated navbar

--- client/src/components/home/navbar.tsx
+++ client/src/components/home/navbar.tsx
@@ -1,15 +1,53 @@
 "use client";
 
+import { setActiveTab } from "@/lib/store/features/tweet-slice";
+import { useAppDispatch, useAppSelector } from "@/lib/store/hook";
+import React, { useEffect } from "react";
 
-function Header() {
+const NavBar: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { activeTab } = useAppSelector((state) => state.tweets);
+
+  useEffect(() => {
+    const status =
+      JSON.parse(localStorage.getItem("status") || "forYou");
+    dispatch(setActiveTab(status));
+  }, [activeTab, dispatch]);
+
   return (
-    <header className="sticky top-0 z-50 bg-black text-white border-b border-gray-700">
-      <div>
-        {/* User Profile */}
-      
+    <div className="mt-14 flex items-center justify-around bg-black text-white h-16 border-b border-gray-700 sm:mt-0">
+      <div className="flex w-1/2 justify-between">
+        <div
+          className={`relative text-sm font-semibold cursor-pointer ${
+            activeTab === "forYou" ? "text-white" : "text-gray-400"
+          }`}
+          onClick={() => {
+            dispatch(setActiveTab("forYou"));
+            localStorage.setItem("status", JSON.stringify("forYou"));
+          }}
+        >
+          For you
+          {activeTab === "forYou" && (
+            <div className="absolute  left-0 right-0 h-1 bg-blue-500 rounded-t-md  top-9"></div>
+          )}
+        </div>
+        <div
+          className={`relative text-sm font-semibold cursor-pointer ${
+            activeTab === "following" ? "text-white" : "text-gray-400"
+          }`}
+          onClick={() => {
+            dispatch(setActiveTab("following"));
+            localStorage.setItem("status", JSON.stringify("following"));
+          }}
+        >
+          Following
+          {activeTab === "following" && (
+            <div className="absolute left-0 right-0 h-1 bg-blue-500 rounded-t-md top-9"></div>
+          )}
+        </div>
       </div>
-    </header>
+    </div>
   );
-}
+};
 
-export default Header;
+export default NavBar;
