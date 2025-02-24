bb005939e4917aa53b45615a711b4e0a7828a8d0
hiba-0325 <hibakm72@gmail.com>
2025-02-24 22:28:00 +0530

    feat(client):home route and explore route

---
+++ client/src/app/(root)/explore/page.tsx
@@ -0,0 +1,8 @@
+import Explore from '@/components/explorer/explorer'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div> <Explore/></div>
+  )
+}
--- client/src/app/(root)/home/page.tsx
+++ client/src/app/(root)/home/page.tsx
@@ -1,10 +1,11 @@
 
-import HomePage from '@/components/home/homePage'
+
+import Home from '@/components/home/homePage'
 import React from 'react'
 
 export default function page() {
   return (
-    <div><HomePage/> 
+    <div><Home/> 
     
     
      </div>
