6701ca69a157758f99137cb798b091a779949663
hiba-0325 <hibakm72@gmail.com>
2025-02-24 22:26:10 +0530

    feat(client): avatar and notification-slice

---
+++ client/src/lib/store/features/tweet-slice.tsx
@@ -0,0 +1,127 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  fetchFollowingUserPost,
+  fetchLikedTweets,
+  // fetchTweetById,
+  fetchTweets,
+  fetchUserTweet,
+} from "../thunks/tweet-thunks";
+import { CommentData, TweetData } from "@/utils/types/types";
+
+
+
+interface TweetsState {
+  tweets: TweetData[];
+  tweet: TweetData | null;
+  followingTweets: TweetData[];
+  userTweet: TweetData[] | null;
+  userLikes: TweetData[] | null;
+  activeTab: "forYou" | "following";
+  comments: CommentData[] | null;
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: TweetsState = {
+  tweets: [],
+  tweet: null,
+  followingTweets: [],
+  userLikes: [],
+  activeTab: "forYou",
+  comments: [],
+  userTweet: null,
+  loading: false,
+  error: null,
+};
+
+const tweetsSlice = createSlice({
+  name: "tweets",
+  initialState,
+  reducers: {
+    setActiveTab: (state, action) => {
+      state.activeTab = action.payload || "forYou";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTweets.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchTweets.fulfilled,
+        (state, action: PayloadAction<TweetData[]>) => {
+          state.loading = false;
+          state.tweets = action.payload;
+        }
+      )
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .addCase(fetchTweets.rejected, (state, action: any) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(fetchFollowingUserPost.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchFollowingUserPost.fulfilled,
+        (state, action: PayloadAction<TweetData[]>) => {
+          state.loading = false;
+          state.followingTweets = action.payload;
+        }
+      )
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .addCase(fetchFollowingUserPost.rejected, (state, action: any) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(fetchUserTweet.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchUserTweet.fulfilled,
+        (state, action: PayloadAction<TweetData[]>) => {
+          state.loading = false;
+          state.userTweet = action.payload;
+        }
+      )
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .addCase(fetchUserTweet.rejected, (state, action: any) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(fetchLikedTweets.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchLikedTweets.fulfilled,
+        (state, action: PayloadAction<TweetData[]>) => {
+          state.loading = false;
+          state.userLikes = action.payload;
+        }
+      )
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .addCase(fetchLikedTweets.rejected, (state, action: any) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      // .addCase(
+      //   fetchTweetById.fulfilled,
+      //   (state, action: PayloadAction<TweetData>) => {
+      //     state.tweet = action.payload;
+      //   }
+      // )
+      // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      // .addCase(fetchTweetById.rejected, (state, action: any) => {
+      //   state.loading = false;
+      //   state.error = action.payload;
+      // });
+  },
+});
+
+export const { setActiveTab } = tweetsSlice.actions;
+
+export default tweetsSlice.reducer;
--- client/src/lib/store/store.ts
+++ client/src/lib/store/store.ts
@@ -1,12 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "@/lib/store/features/auth-slice";
-
+import tweetReducer from "@/lib/store/features/tweet-slice";
+import userReducer from "@/lib/store/features/user-slice";
+import notificationReducer from "@/lib/store/features/notification-slice";
+import chatReducer from "@/lib/store/features/chat-slice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    tweets: tweetReducer,
+    user: userReducer,
+    notification: notificationReducer,
+    chat: chatReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export type AppStore = typeof store; 
+export type AppStore = typeof store;
